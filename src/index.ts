7fbcc7930bef0523e1aa3310aeb7c04698dabb17
Safin Singh <safin.singh@gmail.com>
2022-06-15 00:17:06 +0200

    cmdline

--- src/index.ts
+++ src/index.ts
@@ -1,172 +1,235 @@
 import axios, { AxiosInstance } from "axios";
-import url from "url";
-import querystring from "querystring";
 import dotenv from "dotenv";
-import fs from "fs/promises";
-import { Agent } from "https";
+import https from "node:https";
+import fs from "node:fs";
 
-dotenv.config();
+const RIOT_TLS_CIPHERS = [
+	"TLS_CHACHA20_POLY1305_SHA256",
+	"TLS_AES_128_GCM_SHA256",
+	"TLS_AES_256_GCM_SHA384",
+	"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+];
+const RIOT_AUTHORIZATION_ENDPOINT =
+	"https://auth.riotgames.com/api/v1/authorization";
+const RIOT_ENTITLEMENTS_ENDPOINT =
+	"https://entitlements.auth.riotgames.com/api/token/v1";
+const RIOT_STOREFRONT_ENDPOINT = (puuid: string) =>
+	`https://pd.na.a.pvp.net/store/v2/storefront/${puuid}`;
 
-type Skin = {
-	display_name: string;
-	image: string;
-};
-
-const { __riot_username, __riot_password } = process.env;
-
-function format_seconds(seconds: number) {
-	const h = Math.floor(seconds / 3600)
-		.toString()
-		.padStart(2, "0");
-	const m = Math.floor((seconds % 3600) / 60)
-		.toString()
-		.padStart(2, "0");
-	const s = Math.floor(seconds % 60)
-		.toString()
-		.padStart(2, "0");
-
-	return `${h}:${m}:${s}`;
-}
-
-async function cache_skins_list() {
-	const raw_skins_json = await axios.get(
-		"https://valorant-api.com/v1/weapons/skins"
-	);
-	const skins_to_write: {
-		[id: string]: Skin;
-	} = {};
-	for (const skin of raw_skins_json.data.data) {
-		const skin_level = skin.levels[0];
-		skins_to_write[skin_level.uuid] = {
-			display_name: skin_level.displayName,
-			image: skin_level.displayIcon,
-		};
-	}
-
-	await fs.writeFile("skins.json", JSON.stringify(skins_to_write));
-}
-
-class User {
-	client: AxiosInstance;
-	riot_entitlement: string;
-	riot_token: string;
+type RiotAuthBundle = {
+	riotToken: string;
+	entitlementsToken: string;
 	puuid: string;
+	expiry: Date;
+};
+type RiotAuthTokens = Pick<RiotAuthBundle, "riotToken" | "entitlementsToken">;
 
-	constructor() {
-		const ciphers = [
-			"TLS_CHACHA20_POLY1305_SHA256",
-			"TLS_AES_128_GCM_SHA256",
-			"TLS_AES_256_GCM_SHA384",
-			"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
-		];
-		const agent = new Agent({
-			ciphers: ciphers.join(":"),
-			honorCipherOrder: true,
-			minVersion: "TLSv1.2",
-		});
+// should implement reauth, etc
+interface AuthProvider {
+	getAuthBundle(): Promise<RiotAuthBundle>;
+}
 
-		this.client = axios.create({
+class AuthProviderError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = "AuthProviderError";
+		this.message = message;
+	}
+}
+
+class PasswordAuthProvider implements AuthProvider {
+	username: string;
+	password: string;
+	expiry: Date;
+	bundle?: RiotAuthBundle;
+
+	httpClient: AxiosInstance;
+
+	constructor(username: string, password: string) {
+		this.username = username;
+		this.password = password;
+
+		this.httpClient = axios.create({
 			withCredentials: true,
 			headers: {
 				"User-Agent":
 					"RiotClient/43.0.1.4195386.4190634 rso-auth (Windows; 10;;Professional, x64)",
 			},
-			httpsAgent: agent,
+			httpsAgent: new https.Agent({
+				ciphers: RIOT_TLS_CIPHERS.join(":"),
+				honorCipherOrder: true,
+				minVersion: "TLSv1.2",
+			}),
 		});
-
-		this.riot_entitlement = "";
-		this.riot_token = "";
-		this.puuid = "";
 	}
 
-	async login(username: string, password: string) {
-		const res_a = await this.client.post(
-			"https://auth.riotgames.com/api/v1/authorization",
-			{
+	async getAuthBundle(): Promise<RiotAuthBundle> {
+		if (!this.bundle || this.expiry < new Date()) {
+			const riotToken = await this.getRiotToken();
+			const entitlementsToken = await this.getentitlementsToken(riotToken);
+			const puuid = await this.getPuuid(riotToken);
+
+			const expiry = new Date();
+			expiry.setMinutes(expiry.getMinutes() + 55); // 5 minute grace period
+
+			this.bundle = { riotToken, entitlementsToken, puuid, expiry };
+			return this.bundle;
+		} else {
+			return this.bundle;
+		}
+	}
+
+	async getRiotToken() {
+		const cookie = await this.httpClient
+			.post(RIOT_AUTHORIZATION_ENDPOINT, {
 				client_id: "play-valorant-web-prod",
 				nonce: "1",
 				redirect_uri: "https://playvalorant.com/opt_in",
 				response_type: "token id_token",
-			}
-		);
+			})
+			.then((res) =>
+				res.headers["set-cookie"]?.find((cookie) => cookie.startsWith("asid"))
+			);
+		if (!cookie)
+			throw new AuthProviderError("Failed to retrieve authorization cookies");
 
-		const cookie = res_a.headers["set-cookie"]!.find((elem) =>
-			/^asid/.test(elem)
-		);
-
-		const res = await this.client.put(
-			"https://auth.riotgames.com/api/v1/authorization",
+		const authorizationResponse = await this.httpClient.put(
+			RIOT_AUTHORIZATION_ENDPOINT,
 			{
 				type: "auth",
-				username: username,
-				password: password,
+				username: this.username,
+				password: this.password,
 				remember: true,
 				language: "en_US",
 			},
-			{
-				headers: {
-					Cookie: cookie!,
-				},
-			}
+			{ headers: { cookie } }
 		);
 
-		const { uri } = res.data.response.parameters;
-		const hash = url.parse(uri).hash!.substring(1); // remove leading #
-		const { access_token } = querystring.parse(hash);
+		if (authorizationResponse.data.error) {
+			throw new AuthProviderError(
+				"Authentication failed, check your credentials"
+			);
+		}
+		const authorizationUri = authorizationResponse.data.response.parameters.uri;
+		const authorizationUriHash = new URL(authorizationUri).hash.substring(1); // trim leading #
+		const authorizationToken = new URLSearchParams(authorizationUriHash).get(
+			"access_token"
+		);
+		if (!authorizationToken) {
+			throw new AuthProviderError(
+				"Error retrieving authentication token from authorization URI"
+			);
+		}
 
-		const entitlement = await this.client.post(
-			"https://entitlements.auth.riotgames.com/api/token/v1",
+		return authorizationToken;
+	}
+
+	async getentitlementsToken(authorizationToken: string) {
+		const entitlementsResponse = await this.httpClient.post(
+			RIOT_ENTITLEMENTS_ENDPOINT,
 			{},
-			{ headers: { Authorization: `Bearer ${access_token}` } }
+			{ headers: { Authorization: `Bearer ${authorizationToken}` } }
 		);
+		const entitlementsToken = entitlementsResponse.data.entitlements_token;
+		if (!entitlementsToken)
+			throw new AuthProviderError("Failed to retrieve entitlement token");
+		return entitlementsToken;
+	}
 
-		const userid = await this.client.get(
+	async getPuuid(authorizationToken: string) {
+		const userinfoResponse = await this.httpClient.get(
 			"https://auth.riotgames.com/userinfo",
 			{
-				headers: { Authorization: `Bearer ${access_token}` },
+				headers: { Authorization: `Bearer ${authorizationToken}` },
 			}
 		);
+		const puuid = userinfoResponse.data.sub;
 
-		this.riot_entitlement = entitlement.data.entitlements_token;
-		this.riot_token = access_token as string;
-		this.puuid = userid.data.sub;
+		if (!puuid) throw new AuthProviderError("Failed to retrieve puuid");
+		return puuid;
+	}
+}
+
+type SkinTier = "Select" | "Deluxe" | "Premium" | "Ultra" | "Exclusive";
+type SkinMeta = {
+	displayName: string;
+	price: number;
+	tier: SkinTier;
+	image: string;
+};
+type StorefrontResponse = {
+	offers: {
+		expiration: Date;
+		items: Array<SkinMeta>;
+	};
+};
+
+type RiotStorefrontResponse = {
+	FeaturedBundle: {
+		Bundle: {
+			ID: string;
+			DataAssetID: string;
+			CurrencyID: string;
+			Items: Array<{
+				Item: {
+					ItemTypeID: string;
+					ItemID: string;
+					Amount: number;
+				};
+				BasePrice: number;
+				CurrencyID: string;
+				DiscountPercent: number;
+				DiscountPrice: number;
+				IsPromoItem: boolean;
+			}>;
+			DurationRemainingInSeconds: number;
+			WholesaleOnly: boolean;
+		};
+		BundleRemainingDurationInSeconds: string;
+	};
+	SkinsPanelLayout: {
+		SingleItemOffers: [string, string, string, string];
+		SingleItemOffersRemainingDurationInSeconds: number;
+	};
+};
+
+class Storefront {
+	authProvider: AuthProvider;
+
+	constructor(authProvider: AuthProvider) {
+		this.authProvider = authProvider;
 	}
 
-	async format_store_items(items: string[]) {
-		const skins_raw = await fs.readFile("skins.json", "utf-8");
-		const skins = JSON.parse(skins_raw);
-		return items.map((id) => skins[id] as Skin);
+	async getOffers() {
+		const bundle = await this.authProvider.getAuthBundle();
+		const offers = await this._getOffersRaw(bundle);
+		const skins = offers.SkinsPanelLayout.SingleItemOffers;
+
+		const skinsjson = JSON.parse(fs.readFileSync("skins.json", "utf-8"));
+		console.log(skins.map((uuid) => skinsjson[uuid].display_name));
 	}
 
-	async get_store() {
-		const shop = await this.client.get(
-			`https://pd.na.a.pvp.net/store/v2/storefront/${this.puuid}`,
+	private async _getOffersRaw(bundle: RiotAuthBundle) {
+		const storefrontResponse = await axios.get<RiotStorefrontResponse>(
+			RIOT_STOREFRONT_ENDPOINT(bundle.puuid),
 			{
 				headers: {
-					"X-Riot-Entitlements-JWT": this.riot_entitlement,
-					Authorization: `Bearer ${this.riot_token}`,
+					"X-Riot-Entitlements-JWT": bundle.entitlementsToken,
+					authorization: `Bearer ${bundle.riotToken}`,
 				},
 			}
 		);
-		const skins = shop.data.SkinsPanelLayout;
-
-		return {
-			time_remaining: format_seconds(
-				skins.SingleItemOffersRemainingDurationInSeconds
-			),
-			offers: await this.format_store_items(skins.SingleItemOffers),
-		};
+		const storefrontData = storefrontResponse.data;
+		return storefrontData;
 	}
 }
 
-async function run() {
-	if (!__riot_username || !__riot_password) {
-		console.error("failed to grab creds from .env");
-		return;
-	}
+(async () => {
+	dotenv.config();
+	const { __riot_username, __riot_password } = process.env;
 
-	const me = new User();
-	console.log(await me.get_store());
-}
+	const auth = new PasswordAuthProvider(__riot_username!, __riot_password!);
+	const store = new Storefront(auth);
 
-run();
+	store.getOffers();
+})();
